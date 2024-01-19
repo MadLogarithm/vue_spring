f3f422bd855607d328b4acb2420256bef7d2dd58
MadLogarithm <1141519626@qq.com>
2024-01-19 16:26:45 +0800

    chore(): init main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory} from "vue-router"
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+import HelloWorld from "@/components/HelloWorld.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            component: HelloWorld
+        }
+    ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')

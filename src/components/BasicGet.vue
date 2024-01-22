3c74b048fe78f4b94fba7f4542e0497c0cd0c01c
MadLogarithm <1141519626@qq.com>
2024-01-22 16:37:37 +0800

    chore(): init BasicGet.vue

---
+++ src/components/BasicGet.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "BasicGet",
+  data() {
+    return {
+      responseMessage: ''
+    }
+  },
+  methods: {
+    getHello() {
+      axios.get('http://localhost:8080/hello')
+          .then(response => {
+            this.responseMessage =response.data;
+          })
+          .catch(error => {
+            this.responseMessage = error;
+            console.error('Error fetching hello:', error);
+          });
+    }
+  }
+}
+</script>
+
+<template>
+  <el-button @click="getHello">get</el-button>
+  <p>{{responseMessage}}</p>
+</template>
+
+<style scoped>
+
+</style>

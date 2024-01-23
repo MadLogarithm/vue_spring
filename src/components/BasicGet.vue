34057d2416ca1230e049e11fc17edd9356b19d2c
MadLogarithm <1141519626@qq.com>
2024-01-23 11:06:00 +0800

    feat(): add basic MySQL data get function

--- src/components/BasicGet.vue
+++ src/components/BasicGet.vue
@@ -5,7 +5,8 @@ export default {
   name: "BasicGet",
   data() {
     return {
-      responseMessage: 'null'
+      responseMessage: 'null',
+      userData: []
     }
   },
   methods: {
@@ -21,6 +22,27 @@ export default {
     },
     resetHello() {
       this.responseMessage = 'null';
+    },
+    getUser() {
+      axios.get('http://localhost:8080/getUser')
+          .then(response => {
+            this.userData = this.parseData(response.data);
+          })
+          .catch(error => {
+            console.error('Error fetching getUser:', error);
+          });
+    },
+    resetUser() {
+      this.userData = [];
+    },
+    parseData(data) {
+      return data.map(item => {
+        return {
+          id: item[0],
+          name: item[1],
+          email: item[2]
+        };
+      });
     }
   }
 }
@@ -34,10 +56,21 @@ export default {
   <div id="messageBox">
     <p>{{responseMessage}}</p>
   </div>
+  <div id="testButton">
+    <el-button @click="getUser">get</el-button>
+    <el-button @click="resetUser">reset</el-button>
+  </div>
+  <div id="userTable">
+    <el-table :data="userData" style="width: 70%">
+      <el-table-column prop="id" label="id" />
+      <el-table-column prop="name" label="name" />
+      <el-table-column prop="age" label="age" />
+    </el-table>
+  </div>
 </template>
 
 <style scoped>
-#testButton, #messageBox {
+#testButton, #messageBox, #userTable {
   display: flex;
   justify-content: center;
 }

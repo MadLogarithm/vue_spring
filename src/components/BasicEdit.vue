7b5b072c76fa3f02b473472d63caa35355ed8343
MadLogarithm <1141519626@qq.com>
2024-01-23 15:09:18 +0800

    chore(): init BasicEdit.vue

---
+++ src/components/BasicEdit.vue
@@ -0,0 +1,43 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "BasicEdit",
+  data() {
+    return {
+      userData: []
+    }
+  },
+  methods: {
+    selectAllUser() {
+      axios.get('http://localhost:8080/user/')
+          .then(response => {
+            this.userData = response.data;
+          })
+          .catch(error => {
+            console.error("Error fetching user:", error);
+          });
+    }
+  },
+  created() {
+    this.selectAllUser();
+  }
+}
+</script>
+
+<template>
+  <div id="userTable">
+    <el-table :data="userData" style="width: 70%">
+      <el-table-column prop="id" label="id" />
+      <el-table-column prop="name" label="name" />
+      <el-table-column prop="age" label="age" />
+    </el-table>
+  </div>
+</template>
+
+<style scoped>
+#userTable {
+  display: flex;
+  justify-content: center;
+}
+</style>

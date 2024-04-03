78a2237575d3d89cfb355345b99b70ef7024ddd2
MadLogarithm <1141519626@qq.com>
2024-04-03 18:47:25 +0800

    feat: add CodeTime component to header

--- src/components/HomeSet.vue
+++ src/components/HomeSet.vue
@@ -8,6 +8,19 @@
         <el-col :span="8" style="display: flex; justify-content: center; align-items: center">
           vue_spring
         </el-col>
+        <el-col :span="4" />
+        <el-col :span="4" style="padding: 5px">
+          <el-row>
+            <el-col :span="24">
+              <img ref="https://codetime.dev" alt="CodeTime Badge" src="https://img.shields.io/endpoint?style=plastic&color=222&url=https%3A%2F%2Fapi.codetime.dev%2Fshield%3Fid%3D25076%26project%3D%26in=0">
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <img ref="https://codetime.dev" alt="CodeTime Badge" src="https://img.shields.io/endpoint?style=plastic&color=222&url=https%3A%2F%2Fapi.codetime.dev%2Fshield%3Fid%3D25076%26project%3D%26in=86400000">
+            </el-col>
+          </el-row>
+        </el-col>
       </el-row>
     </el-header>
     <el-container>

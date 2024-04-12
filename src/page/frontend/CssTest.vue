4eec9297b62e5e3b93620bf327e37d5f79e0ba53
MadLogarithm <1141519626@qq.com>
2024-04-12 13:02:42 +0800

    refactor: optimize code by adding loop to reduce redundancy

--- src/page/frontend/CssTest.vue
+++ src/page/frontend/CssTest.vue
@@ -45,15 +45,15 @@ export default {
           </el-row>
           <div style="margin-left: 80px">
             <div class="gridContainer">
-              <div class="gridItem" style="background-color: #7e2f2b"/>
-              <div class="gridItem" style="background-color: #146420"/>
-              <div class="gridItem" style="background-color: #606266"/>
-              <div class="gridItem" style="background-color: blue"/>
-              <div class="gridItem" style="background-color: black"/>
-              <div class="gridItem" style="background-color: blueviolet"/>
-              <div class="gridItem" style="background-color: darkmagenta"/>
-              <div class="gridItem" style="background-color: brown"/>
-              <div class="gridItem" style="background-color: chartreuse"/>
+              <div class="gridItem" />
+              <div class="gridItem" />
+              <div class="gridItem" />
+              <div class="gridItem" />
+              <div class="gridItem" />
+              <div class="gridItem" />
+              <div class="gridItem" />
+              <div class="gridItem" />
+              <div class="gridItem" />
             </div>
           </div>
         </div>
@@ -62,7 +62,7 @@ export default {
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .cssTestContainer {
   height: 590px;
   overflow: auto;
@@ -91,40 +91,18 @@ export default {
   transition: 0.5s;
   .gridItem {}
 }
-.gridContainer:has(.gridItem:nth-child(1):hover) {
-  grid-template-columns: 2fr 1fr 1fr;
-  grid-template-rows: 2fr 1fr 1fr;
-}
-.gridContainer:has(.gridItem:nth-child(2):hover) {
-  grid-template-columns: 1fr 2fr 1fr;
-  grid-template-rows: 2fr 1fr 1fr;
-}
-.gridContainer:has(.gridItem:nth-child(3):hover) {
-  grid-template-columns: 1fr 1fr 2fr;
-  grid-template-rows: 2fr 1fr 1fr;
-}
-.gridContainer:has(.gridItem:nth-child(4):hover) {
-  grid-template-columns: 2fr 1fr 1fr;
-  grid-template-rows: 1fr 2fr 1fr;
-}
-.gridContainer:has(.gridItem:nth-child(5):hover) {
-  grid-template-columns: 1fr 2fr 1fr;
-  grid-template-rows: 1fr 2fr 1fr;
-}
-.gridContainer:has(.gridItem:nth-child(6):hover) {
-  grid-template-columns: 1fr 1fr 2fr;
-  grid-template-rows: 1fr 2fr 1fr;
-}
-.gridContainer:has(.gridItem:nth-child(7):hover) {
-  grid-template-columns: 2fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 2fr;
-}
-.gridContainer:has(.gridItem:nth-child(8):hover) {
-  grid-template-columns: 1fr 2fr 1fr;
-  grid-template-rows: 1fr 1fr 2fr;
-}
-.gridContainer:has(.gridItem:nth-child(9):hover) {
-  grid-template-columns: 1fr 1fr 2fr;
-  grid-template-rows: 1fr 1fr 2fr;
+@for $i from 1 through 9 {
+  .gridItem:nth-child(#{$i}) {
+    background-color: hsl($i* 40, 100%, 74%);
+  }
+  .gridContainer:has(.gridItem:nth-child(#{$i}):hover) {
+    $r: floor(($i - 1) / 3 + 1);
+    $c: ($i - 1) % 3 + 1;
+    $arr: 1fr 1fr 1fr;
+    $rows: set-nth($arr, $r, 2fr);
+    $cols: set-nth($arr, $c, 2fr);
+    grid-template-rows: $rows;
+    grid-template-columns: $cols;
+  }
 }
 </style>
